697084319d86f3db171711e71df8230b69bba577
mirza2018 <mirza.eee.4th@gmail.com>
2026-04-28 14:32:37 +0600

    test 11

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,33 @@
-// import ReactDOM from "react-dom/client";
-// import "./index.css";
-// import { RouterProvider } from "react-router-dom";
-// import router from "./Routes/Routes";
-// import React from "react";
-// import { ConfigProvider } from "antd";
-// import { mainTheme } from "./theme";
-// import Providers from "./redux/lib/Providers";
-// import { Toaster } from "sonner";
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <ConfigProvider theme={mainTheme}>
-//       <Providers>
-//         <Toaster richColors position={"top-center"} />
-//         <RouterProvider router={router} />
-//       </Providers>
-//     </ConfigProvider>
-//   </React.StrictMode>
-// );
-
-import React from "react";
 import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import { store } from "./redux/store";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./Routes/Routes";
+import React from "react";
+import { ConfigProvider } from "antd";
+import { mainTheme } from "./theme";
+import Providers from "./redux/lib/Providers";
+import { Toaster } from "sonner";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <h1 style={{ color: "green", padding: "20px" }}>Redux OK</h1>
-    </Provider>
-  </React.StrictMode>,
+    <ConfigProvider theme={mainTheme}>
+      <Providers>
+        <Toaster richColors position={"top-center"} />
+        <RouterProvider router={router} />
+      </Providers>
+    </ConfigProvider>
+  </React.StrictMode>
 );
+
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { Provider } from "react-redux";
+// import { store } from "./redux/store";
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <h1 style={{ color: "green", padding: "20px" }}>Redux OK</h1>
+//     </Provider>
+//   </React.StrictMode>,
+// );
